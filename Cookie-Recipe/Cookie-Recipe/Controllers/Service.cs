d4b358fdd082c2ce3720823b3821b9866430b362
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-08-22 09:34:41 +0330

    add ingredients List and start app method to Service class.

--- Cookie-Recipe/Cookie-Recipe/Controllers/Service.cs
+++ Cookie-Recipe/Cookie-Recipe/Controllers/Service.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Cookie_Recipe.Input;
+using Cookie_Recipe.Models;
 
 namespace Cookie_Recipe.Controllers
 {
     internal class Service
     {
+        private InputHandler inputHandler;
+        private List<Ingredient> ingredients;
+
+        public Service()
+        {
+            inputHandler = new InputHandler();
+
+            
+            // if a new ingredient is added to the project, an object of it must be added here also.
+            ingredients = new List<Ingredient>
+            {
+                new Butter(),
+                new Sugar(),
+                new WheatFlour(),
+                new CocoaPowder(),
+                new Cinnamon(),
+                new Chocolate(),
+                new Cardamom(),
+                new CoconutFlour()
+            };
+        }
+
+        public void StartApp()
+        {
+            int numberOfIngredients = ingredients.Count;
+            int? id = inputHandler.GetIngredientId(numberOfIngredients);
+
+            if (id == null)
+            {
+                return;
+            }
+
+            int ingredientId = id.Value;
+            
+            // other parts of the program to be completed
+        }
     }
 }
