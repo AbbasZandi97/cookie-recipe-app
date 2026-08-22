c3bb26cc5fc7c971d8c71d7c449a1ab43a4f9911
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-08-22 11:39:49 +0330

    add entry point for whole Progarm in Program class

--- Cookie-Recipe/Cookie-Recipe/Program.cs
+++ Cookie-Recipe/Cookie-Recipe/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Cookie_Recipe
+﻿using Cookie_Recipe.Controllers;
+
+namespace Cookie_Recipe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            new Service().StartApp();
         }
     }
 }
