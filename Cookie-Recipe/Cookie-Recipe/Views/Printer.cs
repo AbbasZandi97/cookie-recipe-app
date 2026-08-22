ef231003e19dbf1d72f513e9b6203e6909556c7d
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-08-22 10:19:24 +0330

    add PrintMenu method to Printer Class

--- Cookie-Recipe/Cookie-Recipe/Views/Printer.cs
+++ Cookie-Recipe/Cookie-Recipe/Views/Printer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cookie_Recipe.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,17 @@ namespace Cookie_Recipe.Views
 {
     internal class Printer
     {
+        
+        public static void PrintMenu(List<Ingredient> ingredients)
+        {
+            Console.WriteLine("Create a new cookie recipe! Available ingredients are: ");
+
+            foreach (var ingredient in ingredients)
+            {
+                Console.WriteLine($"{ingredient.Id}. {ingredient.Name}");
+            }
+        }
+
         public static void PrintEnterInput()
         {
             Console.WriteLine("Enter Id to add ingredient.");
