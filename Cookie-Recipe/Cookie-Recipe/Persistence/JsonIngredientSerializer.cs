6aa7ee3880bf4a3dad38a7b75ce5550d8a62a319
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-08-23 18:44:51 +0330

    change former jsonserializer class's name and implement it

---
+++ Cookie-Recipe/Cookie-Recipe/Persistence/JsonIngredientSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace Cookie_Recipe.Persistence
+{
+    internal class JsonIngredientSerializer : ISerializer
+    {
+        
+        public void Serialize(HashSet<int> ingredientIds)
+        {
+
+            string json = JsonSerializer.Serialize(ingredientIds);
+            File.WriteAllText("ingredientIds.json", json);
+        }
+
+        public HashSet<int> Deserialize()
+        {
+            string json = File.ReadAllText("ingredientIds.json");
+
+            return JsonSerializer.Deserialize<HashSet<int>>(json);
+        }
+
+    }
+}
