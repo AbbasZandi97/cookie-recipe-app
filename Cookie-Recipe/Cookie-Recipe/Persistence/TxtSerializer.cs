915b4e8c35fee1ff8545d38bf081b9acefea795a
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-08-23 18:45:24 +0330

    implement TxtSerializer class

--- Cookie-Recipe/Cookie-Recipe/Persistence/TxtSerializer.cs
+++ Cookie-Recipe/Cookie-Recipe/Persistence/TxtSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cookie_Recipe.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,34 @@ namespace Cookie_Recipe.Persistence
 {
     internal class TxtSerializer : ISerializer
     {
+
+        public void Serialize(HashSet<int> ingredientIds)
+        {
+
+            using StreamWriter writer = new StreamWriter("ingredientIds.txt");
+
+            foreach (int id in ingredientIds)
+            {
+                writer.WriteLine(id);
+            }
+        }
+
+
+        public HashSet<int> Deserialize()
+        {
+            var ids = new HashSet<int>();
+
+            string[] lines = File.ReadAllLines("ingredientIds.txt");
+
+
+            foreach (string line in lines)
+            {
+                ids.Add(int.Parse(line));
+            }
+
+            return ids;
+
+        }
+        
     }
 }
